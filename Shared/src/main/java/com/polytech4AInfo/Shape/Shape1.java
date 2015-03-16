2485428079911309c44835b2e855ced116490902
PReynaud <pierre.reynaud@hotmail.fr>
2015-03-16 08:13:39 +0100

    Modification of the name of the package$

--- Shared/src/main/java/com/polytech4AInfo/Shape/Shape1.java
+++ Shared/src/main/java/com/polytech4AInfo/Shape/Shape1.java
@@ -1,9 +1,9 @@
-package Shape;
+package com.polytech4AInfo.Shape;
 
 /**
  * Created by Pierre on 13/03/2015.
  */
-public class Shape1 extends Shape{
+public class Shape1 extends Shape {
     public Shape1() {
         super(56, 13);
     }
--- Shared/src/main/java/com/polytech4AInfo/Shape/Shape2.java
+++ Shared/src/main/java/com/polytech4AInfo/Shape/Shape2.java
@@ -1,9 +1,9 @@
-package Shape;
+package com.polytech4AInfo.Shape;
 
 /**
  * Created by Pierre on 13/03/2015.
  */
-public class Shape2 extends Shape{
+public class Shape2 extends Shape {
     public Shape2() {
         super(22, 14);
     }
