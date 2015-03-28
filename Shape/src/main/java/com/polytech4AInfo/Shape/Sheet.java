50d0c7e27d380cd44bcc7a22b8f700986a6182d5
PReynaud <pierre.reynaud@hotmail.fr>
2015-03-28 17:04:50 +0100

    Fixes on sheet and tests

--- Shape/src/main/java/com/polytech4AInfo/Shape/Sheet.java
+++ Shape/src/main/java/com/polytech4AInfo/Shape/Sheet.java
@@ -5,9 +5,46 @@ import java.util.ArrayList;
 /**
  * Created by Dimitri on 16/03/2015.
  */
-public class Sheet extends Shape {
+public class Sheet{
+    /**
+     * Longest side of the sheet
+     */
+    protected int length;
+
+    /**
+     * Shortest side of the sheet
+     */
+    protected int breadth;
+
+    /**
+     * List of the different shape which has been placed in the sheet
+     */
     private ArrayList<PlacedShape> content;
+
+    /**
+     * List of the bins which are contained into the sheet
+     */
     private ArrayList<PlacedShape> bins;
+
+    /**
+     * Constructor of the class, a first bin is also created wich is initialized at the size of the sheet
+     * @param length Longest side of the sheet
+     * @param breadth Shortest side of the sheet
+     */
+    public Sheet(int length, int breadth) {
+        if(length > breadth){
+            this.length = length;
+            this.breadth = breadth;
+        }
+        else{
+            this.length = breadth;
+            this.breadth = length;
+        }
+
+        this.content = new ArrayList<PlacedShape>();
+        this.bins = new ArrayList<PlacedShape>();
+        this.bins.add(new PlacedShape(length, breadth));
+    }
 
     public ArrayList<PlacedShape> getContent() {
         return content;
@@ -17,11 +54,12 @@ public class Sheet extends Shape {
         return bins;
     }
 
-    public Sheet(int height, int width) {
-        super(height, width);
-        content = new ArrayList<PlacedShape>();
-        bins = new ArrayList<PlacedShape>();
-        bins.add(new PlacedShape(height, width));
+    public int getLength() {
+        return length;
+    }
+
+    public int getBreadth() {
+        return breadth;
     }
 
     public int putShape(PlacedShape shape) {
