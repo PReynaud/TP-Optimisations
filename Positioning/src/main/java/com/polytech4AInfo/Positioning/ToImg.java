70e31296829bac76d5ae67716de71cc8b4d8d9d7
Dimitri <d.rodarie@gmail.com>
2015-04-13 17:58:05 +0200

    Modified Positioning + test

---
+++ Positioning/src/main/java/com/polytech4AInfo/Positioning/ToImg.java
@@ -0,0 +1,7 @@
+package com.polytech4AInfo.Positioning;
+
+/**
+ * Created by Dimitri on 10/04/2015.
+ */
+public class ToImg {
+}
---
+++ Positioning/src/test/java/com/polytech4AInfo/Positioning/PositioningTest.java
@@ -0,0 +1,22 @@
+package com.polytech4AInfo.Positioning;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dimitri on 09/04/2015.
+ */
+public class PositioningTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testIsPossible() throws Exception {
+
+    }
+}
